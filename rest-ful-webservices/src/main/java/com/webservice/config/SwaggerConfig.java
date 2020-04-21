c9652f0dd29a5c9ddc145c3364a8d0cb78ecb68d
phamvanoanh <pvoanh@cmc.com.vn>
2020-04-21 10:10:31 +0700

    add some information for swagger ui

--- rest-ful-webservices/src/main/java/com/webservice/config/SwaggerConfig.java
+++ rest-ful-webservices/src/main/java/com/webservice/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.webservice.config;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,8 @@ import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +24,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 public class SwaggerConfig {
 	
+	private static final Contact DEFAULT_CONTACT = new Contact("oanhpv", "springboot-microservice", "education");
+	private static final ApiInfo DEFAULT = new ApiInfo(
+			 "Api Documentation",
+			 "Api Documentation", 
+			 "1.0",
+			 "urn:tos",
+	         DEFAULT_CONTACT,
+	         "Apache 2.0",
+	         "http://www.apache.org/licenses/LICENSE-2.0");
+	private static final Set<String> PRODUCES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
+	
 	@Primary
 	@Bean
 	public LinkDiscoverers discoverers() {
@@ -26,10 +42,12 @@ public class SwaggerConfig {
 		plugins.add(new CollectionJsonLinkDiscoverer());
 		return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
 	}
-	 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT)
+				.produces(PRODUCES);
 	}
 	
 }
