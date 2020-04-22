e39eb5cd9fb1de8e6124016bc9f5bba7795080d0
phamvanoanh <pvoanh@cmc.com.vn>
2020-04-22 11:57:15 +0700

    connnet limits-service to config server

--- limits-service/src/main/java/com/webservice/contoller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/webservice/contoller/LimitsConfigurationController.java
@@ -1,6 +1,9 @@
 package com.webservice.contoller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,12 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.webservice.dto.LimitsConfiguration;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @RestController
 public class LimitsConfigurationController {
 	
+	@Autowired private LimitsConfiguration cofig;
+	
 	@RequestMapping(path = "/limits", method = RequestMethod.GET)
 	@ResponseBody
-	public LimitsConfiguration retrieveLimitsFromConfiguration() {
-		return new LimitsConfiguration();
+	public Config retrieveLimitsFromConfiguration() {
+		return new Config(cofig.getMaximum(), cofig.getMinimum());
 	}
+}
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Config {
+	private int maximum;
+	private int minimum;
 }
--- limits-service/src/main/java/com/webservice/dto/LimitsConfiguration.java
+++ limits-service/src/main/java/com/webservice/dto/LimitsConfiguration.java
@@ -1,8 +1,13 @@
 package com.webservice.dto;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
 
 @Data
+@Component
+@ConfigurationProperties("limit-serivce")
 public class LimitsConfiguration {
 	private int maximum;
 	private int minimum;
