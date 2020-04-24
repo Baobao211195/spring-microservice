9483289f5dc10c58108b31ca6863107419ec8492
phamvanoanh <pvoanh@cmc.com.vn>
2020-04-24 11:28:42 +0700

    set up zuul to connect between services

--- currency-conversion-service/src/main/java/com/webservice/service/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/webservice/service/CurrencyExchangeServiceProxy.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service", url = "localhost:8000")
-@FeignClient(name="currency-exchange-service")
+//@FeignClient(name="currency-exchange-service")
+@FeignClient(name="zuul-api-gateway", url = "localhost:8765") // use name of API gateway server
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	
@@ -16,7 +17,8 @@ public interface CurrencyExchangeServiceProxy {
 	 * @param quantity
 	 * @return
 	 */
-	@GetMapping("/currency-converter-feign/from/{from}/to/{to}")
+	//@GetMapping("/currency-converter-feign/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-converter-feign/from/{from}/to/{to}") // forward request from api gateway to service use name of API gateway server
 	public Object convertCurrency(
 			@PathVariable String from,
 			@PathVariable String to);
